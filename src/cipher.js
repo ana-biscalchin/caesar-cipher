e6832226b1223640b5b90b2548a9d01acdd57a94
ana-biscalchin <anacarolinabis@gmail.com>
2019-02-03 02:46:57 -0200

    funciona, faltam pequenos ajustes no css

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,33 @@
+function mudarEstado(primeiro, segundo) {
+      let displayPrimeiro = document.getElementById(primeiro).style.display;
+      let displaySegundo = document.getElementById(segundo).style.display;
+      console.log(displayPrimeiro)
+      if (displayPrimeiro == "none" ) {
+          document.getElementById(primeiro).style.display = "block";
+      }
+      else {
+          document.getElementById(primeiro).style.display = "none"
+      }
+}
+
+document.getElementById("decision_encode").addEventListener("click", showCiphering);
+document.getElementById("decision_decode").addEventListener("click", showDeciphering);
+
+
+function showCiphering(){
+    mudarEstado("ciphering", "deciphering")
+}
+
+function showDeciphering(){
+    mudarEstado("deciphering", "ciphering")
+}
+
+
+
 document.getElementById("encode_button").addEventListener("click", collectEncode);
+document.getElementById("decode_button").addEventListener("click", collectDecode);
+
+
 
 
 function collectEncode() {
@@ -9,7 +38,10 @@ function collectEncode() {
 
     console.log({ msg, offset, encodeResult});
 
-    document.getElementById("cipher_message").innerHTML = encodeResult;
+    document.getElementById("cipher_message").innerHTML =  encodeResult;
+
+    mudarEstado("result_cipher", "ciphering")
+
 }
 
 function cipherEncode(msg, offset) {
@@ -36,8 +68,6 @@ function cipherEncode(msg, offset) {
   return result;
 }
 
-document.getElementById("decode_button").addEventListener("click", collectDecode);
-
 function collectDecode() {
 
     let msg = document.getElementById("second_message").value;
@@ -47,6 +77,8 @@ function collectDecode() {
     console.log({ msg, offset, decodeResult});
 
     document.getElementById("decipher_message").innerHTML = decodeResult;
+
+      mudarEstado("result_deciphering", "deciphering")
 }
 
 function cipherDecode(msg, offset) {
